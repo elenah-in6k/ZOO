831bf4fca21f008a9339e545c2f608a5fe86aad9
ElenaH <elenah@interlink-ua.com>
2015-11-30 10:32:29 +0200

    Delete

--- js/core/Herbivore.js
+++
@@ -1,6 +0,0 @@
-function Herbivore(name, size, givingTongueFrequency, feedingFrequency){
-  Animal.call(this, name, size, givingTongueFrequency, feedingFrequency);
-  this.getAnimalType = function () {
-      return "Herbivore";
-    }
-}
--- js/ng/animalChat.js
+++
@@ -1,3 +0,0 @@
-var animalZoo = angular.module('animalZoo', []);
-
-animalZoo.directive()
