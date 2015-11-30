5044ab4166f3deba5cadb7d570063960d5f0c212
ElenaH <elenah@interlink-ua.com>
2015-11-30 10:21:51 +0200

    Delete

--- test/core/herbivore/Elephant.test.js
+++ test/core/herbivore/Elephant.test.js
@@ -1,12 +1,12 @@
-describe("add herbivore animal ", function() {
+describe("Elephant", function() {
     var elephant = new Elephant("elephant",7,8);
-    it("has name = ", function() {
+    it("has name", function() {
         expect(elephant.getName()).toEqual("elephant")
     });
-    it("has size = ", function() {
+    it("has size ", function() {
         expect(elephant.getSize()).toEqual(25)
     });
-    it("has givingTongueFrequency = ", function() {
+    it("has giving tongue frequency", function() {
         expect(elephant.getGivingTongueFrequency()).toEqual(7)
     });
     it("has feedingFrequency = ", function() {
--- test/core/herbivore/Hare.test.js
+++ test/core/herbivore/Hare.test.js
@@ -1,19 +1,19 @@
 
-describe("add herbivore animal ", function() {
+describe("Hare", function() {
     var hare = new Hare("hare",7,8);
-    it("has name = ", function() {
+    it("has name", function() {
         expect(hare.getName()).toEqual("hare")
     });
-    it("has size = ", function() {
+    it("has size ", function() {
         expect(hare.getSize()).toEqual(3)
     });
-    it("has givingTongueFrequency = ", function() {
+    it("has giving tongue frequency", function() {
         expect(hare.getGivingTongueFrequency()).toEqual(7)
     });
-    it("has feedingFrequency = ", function() {
+    it("has feeding frequency", function() {
         expect(hare.getFeedingFrequency()).toEqual(8)
     });
-    it("has voice = ", function() {
+    it("has voice", function() {
         expect(hare.getVoice()).toEqual("khhrum-hrum")
     });
 
